be28ac40043f814ff9ea2180856842920b4691bc
SridharCR <crsridhar23@gmail.com>
2017-06-23 13:18:21 +0530

    second

--- snakeGame.py
+++ snakeGame.py
@@ -15,4 +15,18 @@ else:
 #Play Surface
 playSurface = pygame.display.set_mode((720, 460))
 pygame.display.set_caption("Snake Game !")
-time.sleep(5)
+#time.sleep(5)
+
+#Color
+red = pygame.Color(255,0,0) #game over
+green = pygame.Color(0,255,0) #snake
+black = pygame.Color(0,0,0) #score
+white = pygame.Color(255,255,255) #background
+brown = pygame.Color(165,42,42) #food
+
+#FPS Controller
+fpsController = pygame.time.Clock()
+
+#Important Variables
+snakePos = [100,50]
+snakeBody = [[100,50],[90,50],[80,50]]
