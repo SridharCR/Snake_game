790aa95290f7bbd5360b6cf795928e1296c6027b
SridharCR <crsridhar23@gmail.com>
2017-06-23 13:57:37 +0530

    third

--- snakeGame.py
+++ snakeGame.py
@@ -30,3 +30,39 @@ fpsController = pygame.time.Clock()
 #Important Variables
 snakePos = [100,50]
 snakeBody = [[100,50],[90,50],[80,50]]
+
+foodPos = [random.randrange(1,72)*10,random.randrange(1,46)*10]
+foodSpawn = True
+
+direction = 'RIGHT'
+changeto = direction
+
+#Gameover function
+def gameOver():
+    myFont = pygame.font.SysFont('monaco',72)
+    GOsurf = myFont.render("Game Over !!",True,red)
+    GOrect = GOsurf.get_rect()
+    GOrect.midtop = (360,15)
+    playSurface.blit(GOsurf,GOrect)
+    pygame.display.flip()
+    time.sleep(4)
+    pygame.quit() #pygame exit
+    sys.exit() #console exit
+
+#Main Logic of the game
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                changeto = "RIGHT"
+            if event.key == pygame.K_RIGHT or event.key == ord('a'):
+                changeto = "LEFT"
+            if event.key == pygame.K_RIGHT or event.key == ord('w'):
+                changeto = "UP"
+            if event.key == pygame.K_RIGHT or event.key == ord('s'):
+                changeto = "DOWN"
+            if event.key == pygame.K_ESCAPE:
+                pygame,event.post(pygame.event.Event(QUIT))
